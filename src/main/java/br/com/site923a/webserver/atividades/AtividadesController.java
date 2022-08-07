6f485b46dc651c86b8de0e4b014a8d57b897af70
Ethan <ysa4@aluno.ifal.edu.br>
2022-08-06 21:37:46 -0300

    trying to add cors again

--- src/main/java/br/com/site923a/webserver/atividades/AtividadesController.java
+++ src/main/java/br/com/site923a/webserver/atividades/AtividadesController.java
@@ -1,13 +1,14 @@
 package br.com.site923a.webserver.atividades;
 
-import java.rmi.activation.UnknownObjectException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.rmi.activation.UnknownObjectException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.site923a.webserver.ConversorStringParaJson;
 
+@CrossOrigin(origins = {"http://localhost:8080", "https://desenv-sistemas-923a.vercel.app/"}, maxAge = 3600)
 @RestController
 public class AtividadesController {
     
