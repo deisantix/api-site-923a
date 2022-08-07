faa626271436af60c9d34099942640f0faf630c8
Ethan <ysa4@aluno.ifal.edu.br>
2022-08-06 21:15:58 -0300

    adding cross origins

--- src/main/java/br/com/site923a/webserver/WebserverApplication.java
+++ src/main/java/br/com/site923a/webserver/WebserverApplication.java
@@ -2,12 +2,26 @@ package br.com.site923a.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserverApplication.class, args);
+	}
+	
+	public WebMvcConfigurer corsConfgurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/atividades")
+					.allowedOrigins("http://localhost:8080", "https://desenv-sistemas-923a.vercel.app/");
+				registry.addMapping("/deletarAtividade")
+					.allowedOrigins("http://localhost:8080", "https://desenv-sistemas-923a.vercel.app/");
+			}
+		};
 	}
 
 }
